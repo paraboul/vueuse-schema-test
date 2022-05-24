b6f96deaeac2c5cc1800e360cee66498584dd970
Harlan Wilton <harlan@harlanzw.com>
2022-05-24 14:37:58 +1000

    fix: @vueuse/schema-org

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import App from './App.vue'
-import { createSSRApp, reactive } from 'vue'
+import { createSSRApp } from 'vue'
 import { createRouter } from './router'
 import { createHead } from '@vueuse/head'
-import { createSchemaOrg } from '@vueuse/schema-org'
+import { createSchemaOrg, useVueUseHead } from '@vueuse/schema-org'
 
 // SSR requires a fresh app instance per request, therefore we export a function
 // that creates a fresh app instance. If using Vuex, we'd also be creating a
@@ -17,9 +17,14 @@ export function createApp() {
   const schemaOrg = createSchemaOrg({
     head,
     canonicalHost: 'https://example.com',
-    useRoute: () => reactive({...router.currentRoute.value})
+    provider: {
+      useRoute: () => router.currentRoute,
+      setupDOM: useVueUseHead(head)
+    }
   })
+  schemaOrg.setupDOM()
   app.use(schemaOrg)
+
 
   app.use(router)
   return { app, router, head }
